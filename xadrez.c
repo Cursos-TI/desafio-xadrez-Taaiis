a4095807f512d8653cb9b06165cc1f74727671c3
Taís Pereira <202503945152@alunos.estacio.br>
2025-11-09 03:54:36 +0000

    Adicionei estrutura de repetição

--- xadrez.c
+++ xadrez.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 
 int main() {
+
+    
+
+    int casas_torre = 5, casas_bispo = 5, casas_rainha = 8;  //Declaracão de váriaveis para o número de casas de cada peça
+
     printf("====== Desafio xadrez! ====== \n");
 
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    
+    for (int i = 0; i < casas_torre; i++)    //Estrutura de repetição
+    {
+        printf("Direita\n"); //Resultado a ser imprimido
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    }
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
+
+    
 
     return 0;
 }
+// Nível Novato - Movimentação das Peças 
+    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover. 
+    // Implementação de Movimentação do Bispo 
+    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal. 
+    // Implementação de Movimentação da Torre 
+    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita. 
+    // Implementação de Movimentação da Rainha 
+    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
