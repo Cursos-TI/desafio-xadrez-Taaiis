63d298e86496b2fd3f43644c20644650a48edcf4
Taís Pereira <202503945152@alunos.estacio.br>
2025-11-10 02:36:20 +0000

    Continuação da estrutura

--- xadrez.c
+++ xadrez.c
@@ -6,15 +6,16 @@ int main() {
 
     int casas_torre = 5, casas_bispo = 5, casas_rainha = 8;  //Declaracão de váriaveis para o número de casas de cada peça
 
-    printf("====== Desafio xadrez! ====== \n");
+    printf("====== Desafio xadrez! ====== \n\n");
 
-    printf ("Casas que a torre irá percorrer... \n\n");
+    printf ("Casas que a torre irá percorrer... \n");
 
     for (int i = 0; i < casas_torre; i++)    //Estrutura de repetição
     {
         printf("Direita\n"); //Resultado a ser impreso
         
-    }printf("\n");
+    }
+    printf("\n");
     
         printf ("Casas que o bispo irá percorrer... \n\n");
     int i = 0;
@@ -27,19 +28,18 @@ int main() {
     }
     printf("\n");
 
+  printf("Casas que a rainha irá percorrer... \n\n");
+//estrutura de repetição
+  int j = 0;
+  do {
+  printf("Esquerda \n");//Resultado a ser impreso
+
+    j++; //incremento para que o programa não entre em loop
+
+  } while (j < casas_rainha);
 
 
 
-
-    
 
     return 0;
 }
-// Nível Novato - Movimentação das Peças 
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover. 
-    // Implementação de Movimentação do Bispo 
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal. 
-    // Implementação de Movimentação da Torre 
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita. 
-    // Implementação de Movimentação da Rainha 
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
